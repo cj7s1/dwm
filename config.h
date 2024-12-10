dc86b99f04dbdf3e6b7f219d446ba98986b39470
cj7s1 <supcolton@gmail.com>
2024-12-10 10:49:36 -0800

    changed icons and changed colors to match background, it made some text hard to read though

--- config.h
+++ config.h
@@ -11,15 +11,15 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=12" };
 static const char dmenufont[]       = "monospace:size=11";
 //background color
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#371990";
 //inactive window border color
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#5527B0";
 //font color
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#360A52";
 //current tag and current window font color
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray4[]       = "#B732BC";
 //Top bar second color (blue) and active window border color
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#280D5E";
 static const unsigned int baralpha = 100;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
@@ -34,7 +34,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "" };
+static const char *tags[] = { "", "2", "3", "4", "5", "6", "7", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
